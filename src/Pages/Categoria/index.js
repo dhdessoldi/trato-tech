370fb76920ea4077d65f41fbfd1b4e9748988ab4
dhdessoldi <dhdessoldi@hotmail.com>
2023-06-21 18:39:06 -0300

    middlewares e toast implementados

--- src/Pages/Categoria/index.js
+++ src/Pages/Categoria/index.js
@@ -1,33 +1,45 @@
 import Header from "components/Header";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from './Categoria.module.scss'
 import Item from "components/Item";
 import Button from "components/Button";
-
+import { useEffect } from "react";
+import { buscarItens } from "store/reducers/itens";
+import { buscarCategorias } from "store/reducers/categorias";
 
 export default function Categoria() {
-  const {nomeCategoria} = useParams();
+
+  const dispatch = useDispatch();
+  const { nomeCategoria } = useParams();
   const navigate = useNavigate();
-  const {categoria, itens} = useSelector(state => {
+
+  const { categoria, itens } = useSelector(state => {
     const regexp = new RegExp(state.busca, 'i')
-    return {    
+    return {
       categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
       itens: state.itens.filter(item => item.categoria === nomeCategoria && item.titulo.match(regexp))
+
     }
   })
 
+  useEffect(() => {
+    dispatch(buscarCategorias())
+    dispatch(buscarItens())
+  }, [dispatch])
+
+
   return (
     <div>
-      <Header 
+      <Header
         titulo={categoria.nome}
         descricao={categoria.descricao}
         imagem={categoria.header}
       >
-        <Button onClick={()=>navigate(`/anuncie/${nomeCategoria}`)}>Quero anunciar!</Button>
+        <Button onClick={() => navigate(`/anuncie/${nomeCategoria}`)}>Quero anunciar!</Button>
       </Header>
       <div className={styles.itens}>
-        {itens?.map(item=>(
+        {itens?.map(item => (
           <Item key={item.id} {...item}></Item>
         ))}
       </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import Router from 'routes';
 import store from './store';
 import { Provider } from 'react-redux';
+import { createStandaloneToast } from '@chakra-ui/toast'
+
+const { ToastContainer } = createStandaloneToast();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router/>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router />
+    <ToastContainer />
+  </Provider>
 );
+
